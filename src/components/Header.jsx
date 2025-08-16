31166ece1c76fdfe18b7ac772ae82fe7ab3796ad
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-08-16 22:02:20 +0200

    Adding Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { PhoneCall } from 'lucide-react';
 import { useState } from 'react';
+import { X, Menu } from 'lucide-react';
 
 
  const navList = [
@@ -16,7 +17,7 @@ const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-      <header className='bg-[#0b2c06] text-white px-8 py-6 flex items-center justify-between'>
+      <header className='bg-green-950 text-white px-8 py-6 flex items-center justify-between'>
         <h1 className='text-xl font-bold tracking-widest'>
             VERDURAS
         </h1>
@@ -33,7 +34,7 @@ const Header = () => {
         </nav>
 
           <div className='hidden md:flex items-center space-x-4'>
-            <PhoneCall className='' />
+            <PhoneCall className='text-amber-400' />
             <a href="#" className='hidden md:inline-block text-sm hover:text-yellow-400'>
                 Book Table
             </a>
@@ -41,11 +42,26 @@ const Header = () => {
 
         <div className='md:hidden'>
             <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-               {isMenuOpen ? 'Close' : 'Menu'}
+               {isMenuOpen ? <X className='text-yellow-400' /> : <Menu className='text-yellow-400' />}
             </button>
         </div>
+
+            {
+                isMenuOpen && (
+                    <div className='absolute top-16 left-0 text-center w-full bg-yellow-200 text-green-800 md:hidden'>
+                        {navList.map((link) => (
+                            <a 
+                                href={link.href}
+                                key={link.href}
+                                className='block py-2 px-4 font-medium hover:text-green-950 transition'
+                                onClick={() => setIsMenuOpen(false)}>
+                                    {link.label}
+                            </a>
+                        ))}
+                    </div>
+            )}
       </header>
-  )
-}
+  );
+};
 
 export default Header
